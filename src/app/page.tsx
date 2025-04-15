b19c208b8373104f06e4973bde445551ca62c121
zqw <1369895031@qq.com>
2025-04-15 23:29:54 +0800

    @测试

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,4 @@
-// 'use client'
-// import { useActionState } from 'react'
-// import { createAction } from './actions'
-// export default function Page() {
-//   const [state, formAction, isPending] = useActionState(createAction, {
-//     errors: {
-//       email: [],
-//       password: []
-//     }
-//   })
-//   const emailState = state?.errors.email ? state?.errors.email[0] : '✔'
-//   const passwordState = state?.errors.password ? state?.errors.password[0] : '✔'
-//   return (
-//     <div>
-//       hello page 08
-//       <form action={formAction}>
-//         <div>
-//           <input type="text" className="border" name="email" /> { emailState }
-//         </div>
-//         <div>
-//           <input type="password" className="border" name="password" /> { passwordState }
-//         </div>
-//         <button type="submit">submit</button>
-//       </form>
-//     </div>
-//   )
-// } 
+
 
 'use client'
 import { useActionState, startTransition } from 'react'
@@ -46,7 +20,7 @@ export default function Page() {
   }
   return (
     <div>
-      hello page 08
+      hello page 
       <form onSubmit={handleSubmit}>
         <div>
           <input type="text" className="border" name="email" /> { emailState }
